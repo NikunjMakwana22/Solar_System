2f403699d603ba1df8a633ce567b7679ca6b4188
NIKUNJ1680022 <nikunjmakwana473@gmail.com>
2021-08-14 18:46:00 +0530

    Added

---
+++ Assets/Scripts/CaameraControl.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CaameraControl : MonoBehaviour
+{
+    Vector3 TouchStart;
+   // public Transform CA;
+    public float ZoomOutMin = 20;
+    public float ZoomOutMax = 52;
+    public Vector3 MaxMoveDistance, MinMoveDistance;
+    public CinemachineVirtualCamera cm;
+    public bool CameraMode=true;
+    public GameObject Parent;
+
+   // public Rigidbody rb;
+  //  public string t;
+
+    
+    void Start()
+    {
+    //    rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Debug.Log(Input.mousePosition);
+        //Debug.Log(Screen.width);
+        //Debug.Log(Screen.height);
+        if (Input.GetMouseButtonDown(0))
+        {
+            TouchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        //if (Input.GetMouseButtonUp(0))
+        //{
+        //    RaycastHit hit;
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    if (Physics.Raycast(ray, out hit, 10000.0f))
+        //    {
+        //        if (Camera.main.ScreenToWorldPoint(Input.mousePosition) == TouchStart)
+        //        {
+        //            t = hit.transform.name.ToString();
+        //        }
+        //    }
+        //}
+        if (Input.touchCount == 2)
+        {
+            Touch TouchZero = Input.GetTouch(0);
+            Touch TouchOne = Input.GetTouch(1);
+
+            Vector2 TouchZeroPrevPos = TouchZero.position - TouchZero.deltaPosition;
+            Vector2 TouchOnePrevPos = TouchOne.position - TouchOne.deltaPosition;
+
+            float PrevMagnitude = (TouchZeroPrevPos - TouchOnePrevPos).magnitude;
+            float CurrentMagnitude = (TouchZero.position - TouchOne.position).magnitude;
+
+            float Difference = CurrentMagnitude - PrevMagnitude;
+            Zoom(-Difference * 0.01f);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = TouchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //  if(CameraMode)
+            //  {
+            //  transform.position += direction;
+            // Vector3 temPos = transform.position;
+            //  temPos.x = Mathf.Clamp(temPos.x, MinMoveDistance.x, MaxMoveDistance.x);
+            //  temPos.y = Mathf.Clamp(temPos.y, MinMoveDistance.y, MaxMoveDistance.y);
+            //  temPos.z = Mathf.Clamp(temPos.z, MinMoveDistance.z, MaxMoveDistance.z);
+            //  transform.position = temPos;
+            transform.position += direction;
+           // }
+            //else
+            //{
+            //    Vector3 TouchEnd = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //    Debug.Log(TouchStart + "      " + TouchEnd);
+            //    if (TouchEnd.x < TouchStart.x)
+            //    {
+            //        Debug.Log("Left");
+            //        Parent.transform.Rotate(new Vector3(0f, 2f, 0f));
+            //    }
+            //    else if(TouchEnd.x > TouchStart.x)
+            //    {
+            //        Debug.Log("Right");
+            //        Parent.transform.Rotate(new Vector3(0f, -2f, 0f));
+            //    }
+            //    //if(direction.y>0)
+            //    //{
+            //    //    Debug.Log("Right");
+            //    //}
+            //    //else
+            //    //{
+            //    //    Debug.Log("Left");
+            //    //}
+            //}
+           // rb.AddForce(direction * 1.5f, ForceMode.Impulse);
+           // StartCoroutine(Incres());
+        }
+        Zoom(Input.GetAxis("Mouse ScrollWheel"));
+
+    }
+    void Zoom(float increment)
+    {
+        cm.m_Lens.OrthographicSize = Mathf.Clamp(cm.m_Lens.OrthographicSize - increment, ZoomOutMin, ZoomOutMax);
+    // Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, ZoomOutMin, ZoomOutMax);
+    }
+    IEnumerator Incres()
+    {
+        yield return new WaitForSeconds(0.7f);
+       // rb.drag = 100;
+        StartCoroutine("stop");
+    }
+    IEnumerator stop()
+    {
+        yield return new WaitForSeconds(0.5f);
+      //  rb.drag = 2.5f;
+        StopAllCoroutines();
+    }
+
+
+    public void SwitchPriority()
+    {
+
+    }
+
+
+    public void ChangeCamMode()
+    {
+        if(CameraMode)
+        {
+            CameraMode = false;
+        }
+        else
+        {
+            CameraMode=true;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,224 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Cinemachine;
+
+public class GameManger : MonoBehaviour
+{
+   // public GameObject dp;
+    //Dropdown m_Dropdown;
+  //  public string[] Names;
+    public string[] Content;
+    public GameObject[] Planet;
+    //public GameObject Mercury;
+    //public GameObject Venus;
+    //public GameObject Earth;
+    //public GameObject Mars;
+    //public GameObject Jupiter;
+    //public GameObject Saturn;
+    //public GameObject Uranus;
+    //public GameObject Neptune;
+   
+    public float speed=1;
+  //  public int days = 1;
+    public bool Play = true;
+  //  public int ModeIndex = 0;
+    public TextMeshProUGUI Speedtext;
+    public TextMeshProUGUI PlayText;
+  //  public TextMeshProUGUI ModeText;
+    public TextMeshProUGUI InfoArea;
+
+    public int priorityIndex = 0;
+    public CinemachineVirtualCamera[] Cams;
+    
+    public Animator trajectoryanim,InfoTextanim;
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(days);
+        if (Play)
+        {
+            Planet[0].transform.Rotate(Vector3.up * 171f * speed * Time.deltaTime);
+            Planet[1].transform.Rotate(Vector3.up * 87.25f * speed * Time.deltaTime);
+            Planet[2].transform.Rotate(Vector3.up * 28f * speed * Time.deltaTime);
+            Planet[3].transform.Rotate(Vector3.up * 12.25f * speed * Time.deltaTime);
+            Planet[4].transform.Rotate(Vector3.up * 2f * speed * Time.deltaTime);
+            Planet[5].transform.Rotate(Vector3.up * 1f * speed * Time.deltaTime);
+            Planet[6].transform.Rotate(Vector3.up * 0.75f * speed * Time.deltaTime);
+            Planet[7].transform.Rotate(Vector3.up * 0.25f * speed * Time.deltaTime);
+        }
+    }
+    public void incrementDays()
+    {
+        //days=days+speed;
+    }
+
+    public void IncreseSpeed(int i)
+    {
+        if (speed != 0)
+        {
+            if (i == 0 && speed < 8)
+            {
+                speed *= 2;
+                Speedtext.text = speed + "X";
+            }
+            else if (i == 1)
+            {
+                speed /= 2;
+                if (speed < 1)
+                {
+                    speed = 1;
+                }
+                Speedtext.text = speed + "X";
+
+            }
+        }
+    }
+    public void togglePlay()
+    {
+        if (Play)
+        {
+            Play = false;
+            PlayText.text = "Play";
+        }
+        else
+        {
+            Play = true;
+            PlayText.text = "Stop";
+        }
+    }
+
+    //public void SwitchPriority(string direction)
+    //{
+
+    //    int len = Cams.Length;
+    //    if(direction=="R")
+    //    {
+    //        if (priorityIndex < 8)
+    //            priorityIndex++;
+    //        else
+    //            priorityIndex = 0;
+
+    //    }
+    //    else if (direction =="L")
+    //    {
+    //        if (priorityIndex > 0)
+    //            priorityIndex--;
+    //        else
+    //            priorityIndex = 8;
+    //    }
+    //    if(priorityIndex == 0)
+    //    {
+    //        speed = 1f;
+    //        Speedtext.text = speed + "X";
+    //        trajectoryanim.SetInteger("trajectoryAnim", 2);
+    //    }
+    //    else
+    //    {
+    //        speed = 0f;
+    //        Speedtext.text = speed + "X";
+    //        trajectoryanim.SetInteger("trajectoryAnim", 1);
+    //    }
+    //    for (int j = 0; j < len; j++)
+    //    {
+    //        if (j == priorityIndex)
+    //            Cams[j].Priority = 1;
+    //        else
+    //            Cams[j].Priority = 0;
+    //    }
+    //    ModeText.text = Names[priorityIndex];
+    //}
+
+
+    public void SetMode(int val)
+    {
+        int len = Cams.Length;
+        priorityIndex = val;
+        if (priorityIndex == 0)
+        {
+            speed = 1f;
+            Speedtext.text = speed + "X";
+            trajectoryanim.SetInteger("trajectoryAnim", 2);
+            //for (int j = 0; j < len-1; j++)
+            //{
+            //    Planet[j].SetActive(true);
+            //}
+            StartCoroutine(PlanetToggle());
+        }
+        else
+        {
+            speed = 0f;
+            Speedtext.text = speed + "X";
+            trajectoryanim.SetInteger("trajectoryAnim", 1);
+        }
+        for (int j = 0; j < len; j++)
+        {
+            if (j == priorityIndex)
+            {
+                Cams[j].Priority = 1;
+            }
+            else
+            {
+                Cams[j].Priority = 0;
+            }
+        }
+        StartCoroutine(PlanetToggle());
+
+
+    }
+    
+
+    public IEnumerator PlanetToggle()
+    {
+        yield return new WaitForSeconds(0f);
+        int len = Cams.Length;
+        if (priorityIndex == 0)
+        {
+            for (int j = 0; j < len - 1; j++)
+            {
+                StopAllCoroutines();
+                StartCoroutine(PlanetShow(0f, j));
+            }
+            InfoTextanim.SetInteger("InfoText", 2);
+        }
+        else
+        {
+            for (int j = 0; j < len - 1; j++)
+            {
+                if (j == priorityIndex - 1)
+                {
+                    StartCoroutine(PlanetShow(0f, j));
+                }
+                else
+                {
+                    StartCoroutine(PlanetHide(1.5f, j));
+                }
+                InfoTextanim.SetInteger("InfoText", 1);
+                InfoArea.text = Content[priorityIndex - 1];
+                
+            }
+        }
+    }
+
+
+    public IEnumerator PlanetShow(float f,int index)
+    {
+        Planet[index].SetActive(true);
+        yield return new WaitForSeconds(f);
+    }
+    public IEnumerator PlanetHide(float f, int index)
+    {
+        yield return new WaitForSeconds(f);
+        Planet[index].SetActive(false);
+    }
+
+}
